0f537ee464b0661b5006d640cb290e5e41f5735f
TheMonster96 <francescopavone525@gmail.com>
2025-10-04 22:31:22 +0200

    Almost completed the basic_wm tutorial. Will have to read its code to implement more stuff and look around for more features. Gotta fix error_code 10

--- src/zwm.zig
+++ src/zwm.zig
@@ -5,7 +5,78 @@ const xlib=@cImport({
 });
 
 
+
+var clients: [10]xlib.Window= undefined;
 var wm_detected: bool= false;
+
+pub fn ConfigureWindow(confreq_event: *xlib.XConfigureRequestEvent, display: ?*xlib.struct__XDisplay) void{
+    var wchanges: xlib.XWindowChanges= .{};
+    wchanges.x = confreq_event.x;
+    wchanges.y = confreq_event.y;
+    wchanges.width = confreq_event.width;
+    wchanges.height = confreq_event.height;
+    wchanges.border_width = confreq_event.border_width;
+    wchanges.sibling = confreq_event.above;
+    wchanges.stack_mode = confreq_event.detail;
+
+    _ = xlib.XConfigureWindow(display, confreq_event.window, @intCast(confreq_event.value_mask), @ptrCast(@constCast(&confreq_event)));
+}
+
+pub fn OnMapRequest(map_request: *xlib.XMapRequestEvent, display: *xlib.struct__XDisplay, root_window: *xlib.Window) void {
+    Frame(&map_request.window, root_window, display);   
+    
+    _ = xlib.XMapWindow(display, map_request.window);
+}
+
+pub fn Frame(window: *xlib.Window,  root_window: *xlib.Window, display: *xlib.struct__XDisplay) void {
+    const BORDER_WIDTH: u4= 3;
+    const BORDER_COLOR: u24= 0xff0000;
+    const BG_COLOR: u24= 0x0000FF;
+    
+    var x_window_attr: xlib.XWindowAttributes= .{};
+    _ = xlib.XGetWindowAttributes(display, window.*, &x_window_attr);
+
+    const frame: xlib.Window = xlib.XCreateSimpleWindow(
+            display,
+            root_window.*,
+            @intCast(x_window_attr.x),
+            x_window_attr.y,
+            @intCast(x_window_attr.width),
+            @intCast(x_window_attr.height),
+            BORDER_WIDTH,
+            BORDER_COLOR,
+            BG_COLOR
+        );
+
+    _ = xlib.XSelectInput(display, root_window.*, xlib.SubstructureRedirectMask | xlib.SubstructureNotifyMask);
+
+    _ = xlib.XAddToSaveSet(display, window.*);
+
+    _ = xlib.XReparentWindow(display, window.*, frame, 0, 0);
+
+    _ = xlib.XMapWindow(display, frame);
+
+    clients[window.*] = frame;
+
+    _ = xlib.XGrabKey(
+            display, 
+            xlib.XKeysymToKeycode(display, xlib.XStringToKeysym("F2")),
+            xlib.Mod1Mask,
+            root_window.*,
+            1,
+            xlib.GrabModeAsync,
+            xlib.GrabModeAsync
+        );
+
+    std.debug.print("Framed a new window\n", .{});
+
+}
+
+
+pub fn CreateSimpleClientWindow(window: *xlib.Window) !void {
+    _ = window;
+    return error.ToBeImplemented;
+}
 
 //TODO: Add a struct or union to encapsulate XErrors so that it's clearer which error occurred and give more detailed information
 
@@ -27,65 +98,70 @@ pub fn OnXError(dpy: ?*xlib.struct__XDisplay, err: [*c]xlib.XErrorEvent) callcon
 
 
 pub fn main() !void {
+    
+    //TODO: Understand why this builtin doesn't work in this case
+    //std.debug.print("{any}", .{@typeInfo(xlib.union__XEvent)});
+    
 
-    var class_hint: xlib.XClassHint= .{
-        .res_name=@as([*c]u8, @constCast(@ptrCast("Zig WM".ptr))),
-        .res_class=@as([*c]u8, @constCast(@ptrCast("ZWM".ptr))),
-    };
+//     var class_hint: xlib.XClassHint= .{
+//      .res_name=@as([*c]u8, @constCast(@ptrCast("Zig WM".ptr))),
+//      .res_class=@as([*c]u8, @constCast(@ptrCast("ZWM".ptr))),
+//  };
 
-    //TODO: Add a struct to encapsulate all the Display/Window core functions to have a more reusable code and to decrease complexity
+    //TODO: Add a struct to encapsulate all the Display/Window core functions to have a more reusable code and to remove unnecessary complexity
     const display: ?*xlib.struct__XDisplay = xlib.XOpenDisplay(null) orelse null;
-    defer _=xlib.XCloseDisplay(display);
+    defer _ = xlib.XCloseDisplay(display);
     
     _ = xlib.XSetErrorHandler(OnWMDetected);
 
-    std.debug.print("ciao\n", .{});
     if(display == null){
         std.debug.print("Display failed to open", .{});
         std.os.linux.exit(1);    
     }
 
-    const colormap: xlib.Colormap= xlib.DefaultColormap(display, 0);
-    var xcolor: xlib.XColor= undefined;
+    const root_window: xlib.Window= xlib.XDefaultRootWindow(@constCast(@ptrCast(display.?)));
 
-    _ = xlib.XParseColor(display, colormap, "#181818", @as([*c]xlib.XColor, @ptrCast(&xcolor)));
-    _ = xlib.XAllocColor(display, colormap, @as([*c]xlib.XColor, @ptrCast(&xcolor)));
-    
+ // const colormap: xlib.Colormap= xlib.DefaultColormap(display, 0);
+ // var xcolor: xlib.XColor= undefined;
 
-    const screen= xlib.XDefaultScreen(display);
-    const root_window= xlib.XRootWindow(display, screen);
-    //const white_pixel= xlib.XWhitePixel(display, screen);
+ // _ = xlib.XParseColor(display, colormap, "#181818", @as([*c]xlib.XColor, @ptrCast(&xcolor)));
+ // _ = xlib.XAllocColor(display, colormap, @as([*c]xlib.XColor, @ptrCast(&xcolor)));
+ // 
 
-    const window= xlib.XCreateSimpleWindow(
-            display,
-            root_window,
-            0, 0,
-            800, 600,
-            1,
-            xlib.XBlackPixel(display, screen),
-            xcolor.pixel,
-        );
+ // const screen= xlib.XDefaultScreen(display);
+ // const root_window= xlib.XRootWindow(display, screen);
+ // //const white_pixel= xlib.XWhitePixel(display, screen);
 
-    defer _= xlib.XDestroyWindow(display, window);
+ // const window: xlib.Window= xlib.XCreateSimpleWindow(
+ //         display,
+ //         root_window,
+ //         0, 0,
+ //         800, 600,
+ //         1,
+ //         xlib.XBlackPixel(display, screen),
+ //         xcolor.pixel,
+ //     );
 
-
-    _ = xlib.XMapWindow(display, window);
-    _ = xlib.XFlush(display);
+ // defer _= xlib.XDestroyWindow(display, window);
 
 
-    var event: xlib.union__XEvent= std.mem.zeroes(xlib.union__XEvent);
+ // _ = xlib.XMapWindow(display, window);
+ // _ = xlib.XFlush(display);
 
-    
-    _ = xlib.XSetClassHint(display, window, &class_hint);
-    _ = xlib.XStoreName(display, window, "Zig WM window".ptr);
 
-    _ = xlib.XSync(display, 0);
-    _ = xlib.XFlush(display);
-    _ = xlib.XSetInputFocus(display, window, xlib.RevertToParent, xlib.CurrentTime);
+  var event: xlib.union__XEvent= std.mem.zeroes(xlib.union__XEvent);
 
-    _ = xlib.XSync(display, 0);
-    _ = xlib.XFlush(display);
-    _ = xlib.XSelectInput(display, window, xlib.KeyPressMask | xlib.ExposureMask);
+ // 
+ // _ = xlib.XSetClassHint(display, window, &class_hint);
+ // _ = xlib.XStoreName(display, window, "Zig WM window".ptr);
+
+ // _ = xlib.XSync(display, 0);
+ // _ = xlib.XFlush(display);
+ // //_ = xlib.XSetInputFocus(display, window, xlib.RevertToNone, xlib.CurrentTime);
+
+ // _ = xlib.XSync(display, 0);
+ // _ = xlib.XFlush(display);
+    _ = xlib.XSelectInput(display, root_window, xlib.SubstructureRedirectMask | xlib.SubstructureNotifyMask);
 
     _ = xlib.XSetErrorHandler(OnXError);
     
@@ -97,6 +173,21 @@ pub fn main() !void {
             xlib.KeyPress => {
                 std.debug.print("Key pressed\n", .{});
                 
+            },
+            xlib.CreateNotify => {
+                std.debug.print("Notified for window creation\n", .{});
+            },
+            xlib.DestroyNotify => {
+                std.debug.print("Notified for window destruction\n", .{});
+            },
+            xlib.ReparentNotify => {
+                std.debug.print("Notified for window reparenting\n", .{});
+            },
+            xlib.ConfigureRequest => {
+            ConfigureWindow(&event.xconfigurerequest, display.?);
+            },
+            xlib.MapRequest => {
+                OnMapRequest(&event.xmaprequest, display.?, @constCast(&root_window));
             },
             else => {}
         }
